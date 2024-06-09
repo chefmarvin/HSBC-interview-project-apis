2b5a96d32ffdbf241dc70a08fb2bc209bf8e0d69
Marvin Lee <marvin@Marvins-MacBook-Pro.local>
2024-06-10 02:26:59 +0800

    refactor: some refactors after code review by copilot

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, HTTPException, Response
 from fastapi.responses import JSONResponse
 from constants import DEFAULT_HISTORY_PERIOD, ORIGINS
 from typing import Dict, Any, List
 from fastapi.middleware.cors import CORSMiddleware
 from model import IYFinanceGenericData, IYFinanceHistoryData
 from finance_data_source.yfinance import YahooFinanceQuery
+import logging
 
 class CustomJSONResponse(JSONResponse):
     def render(self, content: Any) -> bytes:
@@ -18,6 +19,9 @@ class CustomJSONResponse(JSONResponse):
             return {k: CustomJSONResponse.remove_none(v) for k, v in data.items() if v is not None}
         else:
             return data
+
+# create a logger
+logger = logging.getLogger(__name__)
 
 app = FastAPI(default_response_class=CustomJSONResponse)
 
@@ -37,35 +41,30 @@ async def root():
 
 @app.get("/default-symbols-list/", response_model=Dict[str, IYFinanceGenericData])
 async def default_symbols_list():
-    yf_ins = YahooFinanceQuery()
-    yf_ins_data = yf_ins.getSymbolsData()
-    return yf_ins_data
+    try:
+        yf_ins = YahooFinanceQuery()
+        yf_ins_data = yf_ins.getSymbolsData()
+        return yf_ins_data
+    except Exception as e:
+        logger.error(f"Error when get data for default symbols: {e}")
+        raise HTTPException(status_code=500, detail="Error when get data.")
 
 @app.get("/search/by-symbol/{symbol}/", response_model=Dict[str, IYFinanceGenericData])
 async def search_by_symbol(symbol: str):
-    yf_ins = YahooFinanceQuery()
-    yf_ins_data = yf_ins.getSymbolsData([symbol])
-    return yf_ins_data
-
+    try:
+        yf_ins = YahooFinanceQuery()
+        yf_ins_data = yf_ins.getSymbolsData([symbol])
+        return yf_ins_data
+    except Exception as e:
+        logger.error(f"Error when get data for symbol {symbol}: {e}.")
+        raise HTTPException(status_code=500, detail="Error when get data.")
 
 @app.get("/history/by-symbol/{symbol}/", response_model=List[IYFinanceHistoryData])
 async def history_by_symbol(symbol: str, period: str = DEFAULT_HISTORY_PERIOD):
-    """
-    Retrieves the historical data for a given symbol.
-
-    Args:
-        symbol (str): The symbol for which the historical data is to be retrieved.
-        period (str, optional): The period for which the historical data is to be retrieved. Defaults to '1mo'.
-
-    Returns:
-        List[IYFinanceHistoryData]: A list of IYFinanceHistoryData objects, each representing the historical data for the given symbol for a specific date.
-
-    Raises:
-        Exception: If there is an error in fetching the data, an exception is raised and an empty list is returned.
-    """
     try:
         yf_ins = YahooFinanceQuery()
         yf_ins_data = yf_ins.getHistoryBySymbol(symbol, period)
         return yf_ins_data
-    except:
-        return []
+    except Exception as e:
+        logger.error(f"Error when get history data for symbol {symbol}: {e}.")
+        raise HTTPException(status_code=500, detail="Error when get history data.")
